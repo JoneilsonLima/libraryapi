24ad525b28910567540372cb23838ac2315e2a47
joneilson <joneilson84@gmail.com>
2025-07-29 21:33:45 -0300

    feature: adicionando novo endpoint para busca de autor por id

--- src/main/java/com/projeto/libraryapi/controller/AutorController.java
+++ src/main/java/com/projeto/libraryapi/controller/AutorController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/autores")
@@ -34,5 +36,24 @@ public class AutorController {
                 .toUri();
 
         return ResponseEntity.created(location).build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AutorDTO> obterDetalhes(@PathVariable String id) {
+        var idAutor = UUID.fromString(id);
+        Optional<Autor> autorOptional = autorService.obterPorId(idAutor);
+        if (autorOptional.isPresent()) {
+            Autor autor = autorOptional.get();
+            AutorDTO autorDTO = new AutorDTO(
+                    autor.getId(),
+                    autor.getNome(),
+                    autor.getDataNascimento(),
+                    autor.getNacionalidade()
+            );
+
+            return ResponseEntity.ok(autorDTO);
+        }
+
+        return ResponseEntity.notFound().build();
     }
 }
--- src/main/java/com/projeto/libraryapi/service/AutorService.java
+++ src/main/java/com/projeto/libraryapi/service/AutorService.java
@@ -1,9 +1,13 @@
 package com.projeto.libraryapi.service;
 
+import com.projeto.libraryapi.controller.AutorDTO;
 import com.projeto.libraryapi.model.Autor;
 import com.projeto.libraryapi.repository.AutorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class AutorService {
@@ -15,5 +19,9 @@ public class AutorService {
 
     public Autor salvar(Autor autor) {
         return autorRepository.save(autor);
+    }
+
+    public Optional<Autor> obterPorId(UUID id) {
+        return autorRepository.findById(id);
     }
 }
